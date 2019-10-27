bae43ec187e12545ecc559aa2eeeb2ba11cb802e
Yijiang Huang <yijiangh@mit.edu>
2019-10-27 16:57:34 -0400

    Add setup file

---
+++ pddlstream/__version__.py
@@ -0,0 +1,10 @@
+__title__ = 'pddlstream'
+__description__ = 'PDDLStream: Integrating Symbolic Planners and Blackbox Samplers - https://arxiv.org/pdf/1802.08705.pdf'
+__url__ = 'https://github.com/caelan/pddlstream'
+__version__ = '0.0.1'
+__author__ = 'Caelan Garrett'
+__author_email__ = 'caelan@csail.mit.edu'
+__license__ = 'MIT license'
+__copyright__ = 'Copyright 2019 Caelan Garrett'
+
+__all__ = ['__author__', '__author_email__', '__copyright__', '__description__', '__license__', '__title__', '__url__', '__version__']
